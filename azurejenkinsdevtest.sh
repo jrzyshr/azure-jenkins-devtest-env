b8792e834c6f01e86c01fdceca0dd14671d63b26
Peter Laudati <peterlau@microsoft.com>
2017-05-18 01:13:34 +0200

    Removed unused files

--- azurejenkinsdevtest.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-az login
-az account set --subscription {subscription-id}
-
-az group create --name ExampleGroup --location "Central US"
-az group deployment create --name ExampleDeployment --resource-group ExampleGroup --template-file c:\MyTemplates\azuredeploy.json
